ed6e77774ffc9931849024b56e33e4a359827510
Thomas Holder <thomas@thomas-holder.de>
2025-09-12 14:46:49 +0200

    Add test for areLegsAboutTheSame

--- app/src/main/java/org/hwyl/sexytopo/control/util/SurveyUpdater.java
+++ app/src/main/java/org/hwyl/sexytopo/control/util/SurveyUpdater.java
@@ -269,7 +269,7 @@ public class SurveyUpdater {
     }
 
 
-    private static boolean areLegsAboutTheSame(List<Leg> legs) {
+    public static boolean areLegsAboutTheSame(List<Leg> legs) {
 
         for (Leg leg : legs) { // full legs must be unique by definition
             if (leg.hasDestination()) {
